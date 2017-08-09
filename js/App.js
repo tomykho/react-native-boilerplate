7233ba4345144f6833c6d3affe32fb91dc9ed6bc
Tomy Kho <tomy.kho.xwl@gmail.com>
2017-08-09 16:19:40 +0700

    Removed lib styles.js

--- js/App.js
+++ js/App.js
@@ -9,8 +9,6 @@ import {
 import {
   StackNavigator,
 } from 'react-navigation';
-import styles from './lib/styles';
-global.styles = styles;
 import { StyleProvider } from 'native-base';
 import getTheme from '../native-base-theme/components';
 import commonColor from '../native-base-theme/variables/commonColor';
